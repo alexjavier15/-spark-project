97436ea008848e0e019b7966c2a9499b45114d75
Javier Rivas <javieralejandro.rivas@epfl.ch>
2016-05-03 22:24:24 +0200

    added conf for forcing broadcastJoin (test)

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
@@ -147,7 +147,7 @@ private[sql] abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
         Seq(joins.BroadcastHashJoin(
           leftKeys, rightKeys, Inner, BuildLeft, condition, planLater(left), planLater(right)))
 
-      case ExtractEquiJoinKeys(Inner, leftKeys, rightKeys, condition, left, CanBroadcast(right))
+      case ExtractEquiJoinKeys(Inner, leftKeys, rightKeys, condition, left, right)
         if(conf.forceBroadcastJoinEnabled) =>
         Seq(joins.BroadcastHashJoin(
           leftKeys, rightKeys, Inner, BuildRight, condition, planLater(left), planLater(right)))
