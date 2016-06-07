85d46f965548c5f21285bd28d53058d56484f913
Javier Rivas <javieralejandro.rivas@epfl.ch>
2016-06-07 10:12:49 +0200

    close pf files

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/pf/DefaultSource.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/pf/DefaultSource.scala
@@ -17,23 +17,16 @@
 
 package org.apache.spark.sql.execution.datasources.pf
 
-import java.io.PrintStream
-import java.net.{InetAddress, Socket}
-
-import org.apache.hadoop.fs.{FileStatus, Path}
+import org.apache.hadoop.fs.FileStatus
 import org.apache.hadoop.mapreduce.Job
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.deploy.SparkHadoopUtil
-import org.apache.spark.rdd.{RDD, UnionRDD}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.execution.datasources.pf.PFRelation.{NEXT_CHUNK, executeCommand}
 import org.apache.spark.sql.sources._
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.util.SerializableConfiguration
 import org.apache.spark.util.collection.BitSet
-
-import scala.io.BufferedSource
 
 /**
   * Provides access to CSV data from pure SQL statements.
